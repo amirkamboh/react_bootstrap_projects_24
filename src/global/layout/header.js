38f3c53916fd2d897fb2e726af96ab2e003f1afd
amirkamboh <amirkamboh036@gmail.com>
2024-01-29 14:38:48 +0500

    error

--- src/global/layout/header.js
+++ src/global/layout/header.js
@@ -6,13 +6,15 @@ import Navbar from 'react-bootstrap/Navbar';
 const Header = () => {
   return (
     <>
-      <Navbar bg="light" data-bs-theme="light">
+      <Navbar bg="light" data-bs-theme="light" variant='light'>
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Meta Blog</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/blog">Blog</Nav.Link>
+            <Nav.Link href="/single-post">Single Post</Nav.Link>
+            <Nav.Link href="/pages">Pages</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
