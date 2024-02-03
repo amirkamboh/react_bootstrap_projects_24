6ddb64d267c696dabca12a6eca989cfc9b3ce8cd
amirkamboh <amirkamboh036@gmail.com>
2024-02-04 01:51:38 +0500

    Cards Update

--- src/pages/home/Components/latestblog.js
+++ src/pages/home/Components/latestblog.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import { Button,Card } from 'react-bootstrap'
+import Technology from './../../../media/Images/Technology.PNG'
+import Car from './../../../media/Images/Car.PNG'
+import Beach from './../../../media/Images/Beach.PNG'
+import Masque from './../../../media/Images/Masque.PNG'
+import Buzzar from './../../../media/Images/Buzzar.PNG'
+import Mountain from './../../../media/Images/Mountain.PNG'
+import House from './../../../media/Images/House.PNG'
+import Building from './../../../media/Images/Building.PNG'
+import Nature from './../../../media/Images/Nature.PNG'
 
 const Latestblog = () => {
   return (
@@ -7,15 +17,216 @@ const Latestblog = () => {
         <div className="container">
             <div className="row">
                 <div className="col-md-4 col-xs-6">
-                    <div className="card">
-                        <img src='' alt=''></img>
-                        <button type="button" class="btn btn-primary">Technology</button>
-                        <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, adipisci.</h6>
-
-                    </div>
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3 rounded" src={Beach} alt='Beach'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://mdbcdn.b-cdn.net/img/new/avatars/2.webp'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
                 </div>
-                <div className="col-md-4 col-xs-6"></div>
-                <div className="col-md-4 col-xs-6"></div>
+
+                <div className="col-md-4 col-xs-6">
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3 rounded" src={Car} alt='Car'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://picsum.photos/200/300'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                </div>
+
+                <div className="col-md-4 col-xs-6">
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3" src={Masque} alt='Masque'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://picsum.photos/200/301'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                </div>
+
+            </div>
+            <div className="row">
+                <div className="col-md-4 col-xs-6">
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3" src={Buzzar} alt='Buzzar'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://picsum.photos/200'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                </div>
+
+                <div className="col-md-4 col-xs-6">
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3" src={Technology} alt='Technology'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://mdbcdn.b-cdn.net/img/new/avatars/2.webp'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                </div>
+
+                <div className="col-md-4 col-xs-6">
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3" src={Mountain} alt='Mountain'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://picsum.photos/200/305'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                </div>
+
+            </div>
+            <div className="row">
+                <div className="col-md-4 col-xs-6">
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3" src={Nature} alt='Nature'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://mdbcdn.b-cdn.net/img/new/avatars/2.webp'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                </div>
+
+                <div className="col-md-4 col-xs-6">
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3" src={House} alt='House'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://picsum.photos/203'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                </div>
+
+                <div className="col-md-4 col-xs-6">
+                <Card style={{ height: '28rem' }} className='mt-3'>
+                <Card.Img style={{ height: '250px' }} className="pt-3 ps-3 pe-3" src={Building} alt='Building'/>
+                <Card.Body>
+                <Button variant="light text-primary mb-2">Technology</Button>
+                <Card.Text>
+                <h4>The Impact of Technology on the Workplace: How Technology is Changing</h4>
+                <div className='d-flex'>
+               <img
+                     src='https://picsum.photos/200/304'
+                     alt='John Doe'
+                     className='me-2 rounded-circle'
+                     style={{ width: '25px', height: '25px' }}
+               />
+               <div>
+              <p className='text-muted'>Tracey Wilson<small className='ms-4'>February 02, 2024</small></p>
+               </div>
+               </div>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                </div>
+
             </div>
         </div>
       </div>
